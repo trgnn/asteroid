070c23cb3b179346e513c814cb6d58b3fbd50979
troognoon <antoninlaine01@gmail.com>
2025-10-24 16:03:41 +0200

    set FPS target

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from constants import *
-#import constants
 
+# to run the game, use $ uv run main.py
 def main():
-    init_status = pygame.init()
-    print(init_status)
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-       screen.fill("black")
-       pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
 
 
 
